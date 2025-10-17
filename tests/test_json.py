e06984e517e651b4ab099e7283fd14d8783ebfc4
Griger5 <griger04@gmail.com>
2025-10-17 19:01:00 +0200

    add: tests for none values

--- tests/test_json.py
+++ tests/test_json.py
@@ -2,6 +2,14 @@ import pytest
 
 import test_json_ext as t
 
+def test_nljson_none_from_json():
+    json = t.nljson_none_fromjson()
+
+    assert json == None
+
+def test_nljson_none_to_json():
+    t.nljson_none_tojson()
+
 def test_nljson_bool_from_json():
     json = t.nljson_bool_fromjson()
 

f7c30197905cdf0035540fb0f9c0b1b8c88cc908
Griger5 <griger04@gmail.com>
2025-10-21 16:17:02 +0200

    fix: from_cpp method, style changes

--- include/nanobind_json/nanobind_json.h
+++ include/nanobind_json/nanobind_json.h
@@ -19,94 +19,75 @@ namespace nl = nlohmann;
 
 namespace pyjson
 {
-    inline nb::handle from_json(const nl::json &j)
-    {
-        if (j.is_null())
-        {
+    inline nb::object from_json(const nl::json &j) {
+        if (j.is_null()) {
             return nb::none();
         }
-        else if (j.is_boolean())
-        {
+        else if (j.is_boolean()) {
             return nb::bool_(j.get<bool>());
         }
-        else if (j.is_number_integer())
-        {
+        else if (j.is_number_integer()) {
             return nb::int_(j.get<long>());
         }
-        else if (j.is_number_float())
-        {
+        else if (j.is_number_float()) {
             return nb::float_(j.get<double>());
         }
-        else if (j.is_string())
-        {
+        else if (j.is_string()) {
             return nb::str(j.get<std::string>().c_str());
         }
-        else if (j.is_array())
-        {
+        else if (j.is_array()) {
             nb::list obj;
-            for (const auto& el : j)
-            {
+            for (const auto& el : j) {
                 obj.append(from_json(el));
             }
             return std::move(obj);
         }
-        else // Object
-        {
+        else {
             nb::dict obj;
-            for (nl::json::const_iterator it = j.cbegin(); it != j.cend(); ++it)
-            {
+            for (nl::json::const_iterator it = j.cbegin(); it != j.cend(); ++it) {
                 obj[nb::str(it.key().c_str())] = from_json(it.value());
             }
             return std::move(obj);
         }
     }
 
-    inline nl::json to_json(const nb::handle &obj)
-    {
-        if (obj.ptr() == nullptr || obj.is_none())
-        {
+    inline nl::json to_json(const nb::handle &obj) {
+        if (obj.ptr() == nullptr || obj.is_none()) {
             return nullptr;
         }
-        if (nb::isinstance<nb::bool_>(obj))
-        {
+        else if (nb::isinstance<nb::bool_>(obj)) {
             return nb::cast<bool>(obj);
         }
-        if (nb::isinstance<nb::int_>(obj))
-        {
+        else if (nb::isinstance<nb::int_>(obj)) {
             return nb::cast<long>(obj);
         }
-        if (nb::isinstance<nb::float_>(obj))
-        
+        else if (nb::isinstance<nb::float_>(obj)) {
             return nb::cast<double>(obj);
-        
-        if (nb::isinstance<nb::bytes>(obj))
-        {
+        }
+        else if (nb::isinstance<nb::bytes>(obj)) {
             nb::module_ base64 = nb::module_::import_("base64");
             return nb::cast<std::string>(base64.attr("b64encode")(obj).attr("decode")("utf-8"));
         }
-        if (nb::isinstance<nb::str>(obj))
-        {
+        else if (nb::isinstance<nb::str>(obj)) {
             return nb::cast<std::string>(obj);
         }
-        if (nb::isinstance<nb::tuple>(obj) || nb::isinstance<nb::list>(obj))
-        {
+        else if (nb::isinstance<nb::tuple>(obj) || nb::isinstance<nb::list>(obj)) {
             auto out = nl::json::array();
-            for (const nb::handle value : obj)
-            {
+            for (const nb::handle value : obj) {
                 out.push_back(to_json(value));
             }
             return out;
         }
-        if (nb::isinstance<nb::dict>(obj))
-        {
+        else if (nb::isinstance<nb::dict>(obj)) {
             auto out = nl::json::object();
-            for (const nb::handle key : obj)
-            {
+            for (const nb::handle key : obj) {
                 out[nb::cast<std::string>(nb::str(key))] = to_json(obj[key]);
             }
             return out;
         }
-        throw std::runtime_error("to_json not implemented for this type of object: " + nb::cast<std::string>(nb::repr(obj)));
+        else {
+            throw std::runtime_error("to_json not implemented for this type of object: " + nb::cast<std::string>(nb::repr(obj)));
+        }
     }
 }
 
@@ -169,15 +150,14 @@ public:
             value = pyjson::to_json(src);
             return true;
         }
-        catch (...)
-        {
+        catch (...) {
             return false;
         }
     }
 
     static handle from_cpp(const nl::json &src, rv_policy policy, cleanup_list *cleanup) noexcept {
-        handle obj = pyjson::from_json(src);
-        return obj;
+        object obj = pyjson::from_json(src);
+        return obj.release();
     }
 };
 
